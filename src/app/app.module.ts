dc2d14f841033201a2317e8ea9f3e5ef83e62cbd
Samuel-Kennedy <samuel.gomes@aol.com>
2024-08-18 19:02:01 -0300

    conteudo-rick-inicio cria conteudo-rick-inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderRickComponent } from './header-rick/header-rick.component';
 import { HomeComponent } from './home/home.component';
+import { InicioRickComponent } from './conteudo-rick/inicio-rick/inicio-rick.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderRickComponent,
-    HomeComponent
+    HomeComponent,
+    InicioRickComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/conteudo-rick/inicio-rick/inicio-rick.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-rick',
+  templateUrl: './inicio-rick.component.html',
+  styleUrls: ['./inicio-rick.component.css']
+})
+
+export class InicioRickComponent implements OnInit{
+
+  srcImagem: string = '../assets/inicio-img-rick.png';
+
+  ngOnInit(): void {
+    
+  }
+
+}
