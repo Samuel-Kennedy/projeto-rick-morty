dd5cdd859f81f4db1cfe92d5190d5a1142047e82
Samuel-Kennedy <samuel.gomes@aol.com>
2024-08-19 18:09:36 -0300

    conteudo-rick feat:implementar funcionalidade paginator

--- src/app/conteudo-rick/inicio-rick/inicio-rick.component.ts
+++ src/app/conteudo-rick/inicio-rick/inicio-rick.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { RequisicaoRickService } from '../service/requisicao-rick.service';
 
 @Component({
@@ -6,23 +8,34 @@ import { RequisicaoRickService } from '../service/requisicao-rick.service';
   templateUrl: './inicio-rick.component.html',
   styleUrls: ['./inicio-rick.component.css']
 })
-
 export class InicioRickComponent implements OnInit {
-
   srcImagem: string = '../assets/inicio-img-rick.png';
-  srcImagemGeral: any;
-  constructor(
-    private requisicaoRickService: RequisicaoRickService
-  ){}
+  srcImagemGeral: string[] = [];
+  dataSource = new MatTableDataSource<string>();
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+
+  constructor(private requisicaoRickService: RequisicaoRickService) {}
 
   ngOnInit(): void {
-    this.requisicaoRickService.getCharacters().subscribe({
-      next: (data) => {
-        console.log(data);
-        this.srcImagemGeral = data.results.map((character: any) => character.image);
-        console.log(this.srcImagemGeral);
-      }
-    })
+    this.loadImages();
   }
 
+  loadImages(): void {
+    this.requisicaoRickService.getCharacters().subscribe({
+      next: (data) => {
+        this.srcImagemGeral = data.results.map((character: any) => character.image);
+        this.dataSource.data = this.srcImagemGeral;
+        this.setupPaginator();
+      },
+      error: () => {
+        // Handle error
+      }
+    });
+  }
+
+  setupPaginator(): void {
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
+  }
 }
